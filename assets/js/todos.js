19b571d5d631ca2009f73ed466ffabed7121418d
Phillip Rognerud <philliprognerud2@yahoo.com>
2017-06-15 23:52:22 -0700

    final touches on the app

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,26 @@
 //check off specific todos by clicking
-$("li").click(function (){
+$("ul").on("click", "li", function (){
   $(this).toggleClass("completed");
 });
 
 //click on X to delete todo item
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+$("input[type=text]").keypress(function(event){
+  if(event.which === 13){
+    //grab new todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+    //create a new li and add to ul
+    $("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + todoText + "</li>");
+  }
+});
+
+$(".fa-plus").click(function(){
+  $("input[type=text]").fadeToggle();
 });
